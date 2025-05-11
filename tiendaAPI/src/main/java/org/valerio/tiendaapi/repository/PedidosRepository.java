e5e843891f1035999dfc775facf7228c4f7ec3ba
carlosValerio5 <carlitomanuelvalri@hotmail.com>
2025-05-11 12:33:45 -0600

    Cambios a pedidosRepository

--- tiendaAPI/src/main/java/org/valerio/tiendaapi/repository/PedidosRepository.java
+++ tiendaAPI/src/main/java/org/valerio/tiendaapi/repository/PedidosRepository.java
@@ -1,10 +1,11 @@
 package org.valerio.tiendaapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.valerio.tiendaapi.model.Clientes;
 import org.valerio.tiendaapi.model.Pedidos;
 
 import java.util.List;
 
 public interface PedidosRepository extends JpaRepository<Pedidos, Integer> {
-    List<Pedidos>findByClienteId(Integer clienteId);
+    List<Pedidos>findByCliente(Clientes cliente);
 }
