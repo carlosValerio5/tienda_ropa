62d07cc44bfe7fa172ba7d6be091de36f12f276a
carlosValerio5 <carlitomanuelvalri@hotmail.com>
2025-05-11 12:45:07 -0600

    Añadi algunos metodos para acceder a funciones y stored procedures de la base de datos pero aun no puedo probar si funcionan porque hay errores en otras secciones

--- tiendaAPI/src/main/java/org/valerio/tiendaapi/repository/ClientesRepository.java
+++ tiendaAPI/src/main/java/org/valerio/tiendaapi/repository/ClientesRepository.java
@@ -1,8 +1,14 @@
 package org.valerio.tiendaapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 import org.valerio.tiendaapi.model.Clientes;
 
 public interface ClientesRepository extends JpaRepository<Clientes, Integer> {
+
+    @Query(nativeQuery = true, value="SELECT get_cliente_nombre_completo(:clienteId)")
+    public String getClienteNombreCompleto(@Param("clienteId") Integer clienteId);
 
 }
--- tiendaAPI/src/main/java/org/valerio/tiendaapi/repository/PedidosRepository.java
+++ tiendaAPI/src/main/java/org/valerio/tiendaapi/repository/PedidosRepository.java
@@ -1,6 +1,7 @@
 package org.valerio.tiendaapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.valerio.tiendaapi.model.Clientes;
 import org.valerio.tiendaapi.model.Pedidos;
 
@@ -8,4 +9,7 @@ import java.util.List;
 
 public interface PedidosRepository extends JpaRepository<Pedidos, Integer> {
     List<Pedidos>findByCliente(Clientes cliente);
+
+    @Procedure("crear_pedido")
+    void crearPedido(Integer pClienteID, String pEstado, Integer[] pProductos, Integer pCantidades);
 }
